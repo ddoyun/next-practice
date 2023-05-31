1b78039f8ddcb916afe60078bc04f10ba3b2b0f4
soyun <soyoon4122@naver.com>
2023-06-01 01:52:23 +0900

    Feat: 서버 사이드 렌더링시 스타일 적용 코드 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,50 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+// styled-components 적용할 때 HTML만 로딩되고 스타일은 미적용
+// 코드를 추가해야 서버 사이드 렌더링 시 styled-components 헤더에 주입
+// 즉 서버에서 미리 HTML을 마크업할 때 스타일도 HTML 요소에 녹여냄
+class MyDocument extends Document {
+  static async loadGetInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const intialProps = await Document.getInitialProps(ctx);
+      return {
+        ...intialProps,
+        styles: (
+          <>
+            {intialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
