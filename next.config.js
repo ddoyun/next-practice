1d28c95293a316dab2a755ae27c7b36260631506
soyun <soyoon4122@naver.com>
2023-06-01 01:50:37 +0900

    Feat: svg 포맷 webpack 로더 추가

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// svg 포맷을 다루기 위해 webpack 로더 추가
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  }
+}
