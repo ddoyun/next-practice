0895f8c29a0262b1e5865f0e0f7102563b8ed967
soyun <soyoon4122@naver.com>
2023-06-03 00:49:15 +0900

    Feat: Catch-All 라우터 적용

--- pages/blog/[...slug].tsx
+++ pages/blog/[...slug].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 
-// Catch-All 라우트 어떤 경로이며, 많은 세그먼트를
+// Catch-All 라우터는 어떤 경로이며, 많은 세그먼트를
 // 갖는지에 상관없이 동일한 컴포넌트를 불러올 수 있음
 const BlogPostsPage = () => {
   const router = useRouter();
