98ed1ccb80fcce6e08a77a5e847ab190d48ce574
soyun <soyoon4122@naver.com>
2023-06-03 00:47:15 +0900

    Feat: 404 페이지 생성

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// pages 폴더에 404로 된 파일을 생성하면 404 설정이 알아서 됨
+const NotfoundPage = () => {
+  return (
+    <div>
+      <h1>Not Found Page!!</h1>
+    </div>
+  );
+};
+
+export default NotfoundPage;
