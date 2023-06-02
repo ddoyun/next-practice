cc030fc19e079ea2f518aa6eec8786f9c4f1b23d
soyun <soyoon4122@naver.com>
2023-06-02 23:52:02 +0900

    Fix: html 태그 삭제

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,9 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from "next/document";
+import Document, { DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 // styled-components 적용할 때 HTML만 로딩되고
 // 스타일은 헤더에 미적용이기 때문에 SRR을 위해 아래 코드 추가
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async loadGetInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -19,31 +13,13 @@ class MyDocument extends Document {
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
-      const intialProps = await Document.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx);
       return {
-        ...intialProps,
-        styles: (
-          <>
-            {intialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
     } finally {
       sheet.seal();
     }
   }
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
 }
-
-export default MyDocument;
