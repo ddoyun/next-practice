ed29801464ac53c2eef190f24a064484f0331668
soyun <soyoon4122@naver.com>
2023-06-02 23:52:51 +0900

    Feat: styled-components 적용

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return "hi";
-}
+import type { NextPage } from "next";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: pink;
+`;
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Title>Hello world</Title>
+    </div>
+  );
+};
+
+export default Home;
